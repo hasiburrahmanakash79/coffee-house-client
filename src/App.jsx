a9284240d15e8fc61ccdc2dc7ed3f55846d96b27
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-05-11 11:47:19 +0600

    delete mathod apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
+import CoffeeCard from './components/CoffeeCard'
 
 function App() {
+  const coffeeCollection = useLoaderData()
 
   return (
-    <div>
+    <div className='container mx-auto my-6 p-5'>
       <div className='flex gap-4'>
         <Link to="/">Home</Link>
         <Link to="/addCoffee">Add Coffee</Link>
         <Link to="/updateCoffee">Update Coffee</Link>
       </div>
       <h1 className='text-5xl '>Coffee House</h1>
+      <p>Type of Coffee in our shop: {coffeeCollection.length} type.</p>
+      <div className='md:grid grid-cols-3 gap-5'>
+        {
+          coffeeCollection.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+        }
+      </div>
     </div>
   )
 }
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,64 @@
+import { FaEye, FaEdit, FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+  const { coffeeName, photo, supplier, category, _id, test, details } = coffee;
+
+    const handleDelete = (_id) =>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirm'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              
+            fetch(`http://localhost:5000/coffee/${_id}`,{
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                   Swal.fire(
+                    'Deleted!',
+                    'Your Coffee has been deleted.',
+                    'success'
+                  ) 
+                }
+                
+            })
+            }
+          })
+    }
+
+
+  return (
+    <div className="">
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img src={photo} alt="coffee" className="w-36" />
+        </figure>
+        <div className="card-body flex">
+          <div>
+            <h2 className="card-title">{coffeeName}</h2>
+            <p>Details: {details}</p>
+            <p>Supplier: {supplier}</p>
+            <p>Category: {category}</p>
+            <p>Test: {test}</p>
+          </div>
+          <div className="flex gap-4">
+            <button><FaEye></FaEye></button>
+            <button><FaEdit></FaEdit></button>
+            <button onClick={() => handleDelete(_id)}><FaTrashAlt></FaTrashAlt></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
